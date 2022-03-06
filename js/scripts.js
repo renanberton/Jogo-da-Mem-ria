29bb0acd730271d857bfb04d46c5be16e576961e
renanberton <renan_berton@yahoo.com.br>
2022-03-05 21:51:10 -0300

    First Commit

---
+++ js/scripts.js
@@ -0,0 +1,175 @@
+let bulbasauroCard = document.getElementById('bulbasaur');
+let charmanderCard = document.getElementById('charmander');
+let pikachuCard = document.getElementById('pikachu');
+let onixCard = document.getElementById('onix');
+let snorlaxCard = document.getElementById('snorlax');
+let squirtleCard = document.getElementById('squirtle');
+let zapdosCard = document.getElementById('zapdos');
+let gengarCard = document.getElementById('gengar');
+let bulbasauroCard1 = document.getElementById('bulbasaur1');
+let charmanderCard1 = document.getElementById('charmander1');
+let pikachuCard1 = document.getElementById('pikachu1');
+let onixCard1 = document.getElementById('onix1');
+let snorlaxCard1 = document.getElementById('snorlax1');
+let squirtleCard1 = document.getElementById('squirtle1');
+let zapdosCard1 = document.getElementById('zapdos1');
+let gengarCard1 = document.getElementById('gengar1');
+
+bulbasauroCard.addEventListener('click', (e) => {
+    if(bulbasauroCard.classList.contains('bulba') && bulbasauroCard1.classList.contains('bulba')) {
+        bulbasauroCard.classList.add('bulba');
+        bulbasauroCard1.classList.add('bulba');
+    } else {
+    bulbasauroCard.classList.toggle('bulba');        
+    }
+});
+
+bulbasauroCard1.addEventListener('click', (e) => {
+    if(bulbasauroCard1.classList.contains('bulba') && bulbasauroCard.classList.contains('bulba')) {
+        bulbasauroCard1.classList.add('bulba');
+        bulbasauroCard.classList.add('bulba');
+    } else {
+    bulbasauroCard1.classList.toggle('bulba');        
+    }
+});
+
+charmanderCard.addEventListener('click', (e) => {
+    if(charmanderCard.classList.contains('charmander') && charmanderCard1.classList.contains('charmander')) {
+        charmanderCard.classList.add('charmander');
+        charmanderCard1.classList.add('charmander');
+    } else {
+    charmanderCard.classList.toggle('charmander');        
+    }
+});
+
+
+charmanderCard1.addEventListener('click', (e) => {
+        if(charmanderCard1.classList.contains('charmander') && charmanderCard.classList.contains('charmander')) {
+        charmanderCard1.classList.add('charmander');
+        charmanderCard.classList.add('charmander');
+    } else {
+    charmanderCard1.classList.toggle('charmander');        
+    }
+});
+
+pikachuCard.addEventListener('click', (e) => {
+    if(pikachuCard.classList.contains('pikachu') && pikachuCard1.classList.contains('pikachu')) {
+        pikachuCard.classList.add('pikachu');
+        pikachuCard1.classList.add('pikachu');
+    } else {
+    pikachuCard.classList.toggle('pikachu');        
+    }
+});
+
+pikachuCard1.addEventListener('click', (e) => {
+    if(pikachuCard1.classList.contains('pikachu') && pikachuCard.classList.contains('pikachu')) {
+        pikachuCard1.classList.add('pikachu');
+        pikachuCard.classList.add('pikachu');
+    } else {
+    pikachuCard1.classList.toggle('pikachu');        
+    }
+});
+
+onixCard.addEventListener('click', (e) => {
+    if(onixCard.classList.contains('onix') && onixCard1.classList.contains('onix')) {
+        onixCard.classList.add('onix');
+        onixCard1.classList.add('onix');
+    } else {
+    onixCard.classList.toggle('onix');        
+    }
+});
+
+onixCard1.addEventListener('click', (e) => {
+    if(onixCard1.classList.contains('onix') && onixCard.classList.contains('onix')) {
+        onixCard1.classList.add('onix');
+        onixCard1.classList.add('onix');
+    } else {
+    onixCard1.classList.toggle('onix');   
+    }
+})
+
+snorlaxCard.addEventListener('click', (e) => {
+    if(snorlaxCard.classList.contains('snorlax') && snorlaxCard1.classList.contains('snorlax')) {
+        snorlaxCard.classList.add('snorlax');
+        snorlaxCard1.classList.add('snorlax');
+    } else {
+        snorlaxCard.classList.toggle('snorlax');  
+    }
+})
+
+
+snorlaxCard1.addEventListener('click', (e) => {
+    if(snorlaxCard1.classList.contains('snorlax') && snorlaxCard.classList.contains('snorlax')) {
+        snorlaxCard1.classList.add('snorlax');
+        snorlaxCard.classList.add('snorlax');
+    } else {
+    snorlaxCard1.classList.toggle('snorlax');   
+    }
+})
+
+squirtleCard.addEventListener('click', (e) => {
+    if(squirtleCard.classList.contains('squirtle') && squirtleCard1.classList.contains('squirtle')) {
+        squirtleCard.classList.add('squirtle');
+        squirtleCard1.classList.add('squirtle');
+    } else {
+        squirtleCard.classList.toggle('squirtle');  
+    }
+})
+
+squirtleCard1.addEventListener('click', (e) => {
+    if(squirtleCard1.classList.contains('squirtle') && squirtleCard.classList.contains('squirtle')) {
+        squirtleCard1.classList.add('squirtle');
+        squirtleCard.classList.add('squirtle');
+    } else {
+        squirtleCard1.classList.toggle('squirtle');  
+    }
+})
+
+
+zapdosCard.addEventListener('click', (e) => {
+    if(zapdosCard.classList.contains('zapdos') && zapdosCard1.classList.contains('zapdos')) {
+        zapdosCard.classList.add('zapdos');
+        zapdosCard1.classList.add('zapdos');
+    } else {
+        zapdosCard.classList.toggle('zapdos');  
+    }})
+
+zapdosCard1.addEventListener('click', (e) => {
+    if(zapdosCard1.classList.contains('zapdos') && zapdosCard.classList.contains('zapdos')) {
+        zapdosCard1.classList.add('zapdos');
+        zapdosCard.classList.add('zapdos');
+    } else {
+        zapdosCard1.classList.toggle('zapdos');  
+    }
+})
+
+gengarCard.addEventListener('click', (e) => {
+    if(gengarCard.classList.contains('gengar') && gengarCard1.classList.contains('gengar')) {
+        gengarCard.classList.add('gengar');
+        gengarCard1.classList.add('gengar');
+    } else {
+        gengarCard.classList.toggle('gengar');  
+    }
+})
+
+gengarCard1.addEventListener('click', (e) => {
+    if(gengarCard1.classList.contains('gengar') && gengarCard1.classList.contains('gengar')) {
+        gengarCard1.classList.add('gengar');
+        gengarCard1.classList.add('gengar');
+    } else {
+        gengarCard1.classList.toggle('gengar');  
+    }})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
